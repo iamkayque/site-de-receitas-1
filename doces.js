a5a084d898b80aade567884a027d6b5965bf2c9f
Kayque <hkayque430@gmail.com>
2024-12-06 23:39:19 -0300

    done

--- doces.js
+++ doces.js
@@ -1,79 +1,25 @@
-let Ingredientes1 = document.getElementById('itm1');
-let quantidade1 = document.getElementById('qtd1');
-let preco1 = document.getElementById('preco1');
-let total1 = document.getElementById('tot1');
+function calcular() {
+    //selecione todasd as linhasd da tabela
+    const linhas = document.querySelectorAll("table tr");
+    let totalGeral = 0; //variavel para amarzenar o total geral
 
-let Ingredientes2 = document.getElementById('itm2');
-let quantidade2 = document.getElementById('qtd2');
-let preco2 = document.getElementById('preco2');
-let total2 = document.getElementById('tot2');
+    //itera sobre as linhas da tabela
+    linhas.forEach((linha, index) => {
+        if (index > 0 && index < linhas.length - 1) {
 
-let Ingredientes3 = document.getElementById('itm3');
-let quantidade3 = document.getElementById('qtd3');
-let preco3 = document.getElementById('preco3');
-let total3 = document.getElementById('tot3');
+            const preco = linhas.querySelector(".preco").value;
 
-let Ingredientes4 = document.getElementById('itm4');
-let quantidade4 = document.getElementById('qtd4');
-let preco4 = document.getElementById('preco4');
-let total4 = document.getElementById('tot4');
+            const quantidade = linha.querySelector(".qtd").value;
 
+            const total = preco * quantidade || 0;
 
+            linha.querySelector(".tot").textContent = total.toFixed(2);
 
+            totalGeral += total; 
 
+        }
 
+    });
 
-let ValorFinal = document.getElementById('totalFinal')
-//criando a função de calculo do 1 produto
-
-function calcularTotal1() {
-    let qtd = parseFloat(quantidade1.value) || 0;
-    let preco = parseFloat(preco1.value) || 0;
-    total1.textContent = (qtd * preco).toFixed(2); //tofixed() determina a qtd de casas decimais que desejo representar no numero
-    atualiizarvalorFinal();
+    document.getElementById("totalFinal").textContent = totalGeral.toFixed(2);
 }
-
-function calcularTotal2() {
-    let qtd = parseFloat(quantidade2.value) || 0;
-    let preco = parseFloat(preco2.value) || 0;
-    total2.textContent = (qtd * preco).toFixed(2); //tofixed() determina a qtd de casas decimais que desejo representar no numero
-    atualiizarvalorFinal();
-}
-
-
-function calcularTotal3() {
-    let qtd = parseFloat(quantidade3.value) || 0;
-    let preco = parseFloat(preco3.value) || 0;
-    total3.textContent = (qtd * preco).toFixed(2); //tofixed() determina a qtd de casas decimais que desejo representar no numero
-    atualiizarvalorFinal();
-}
-
-
-function calcularTotal4() {
-    let qtd = parseFloat(quantidade4.value) || 0;
-    let preco = parseFloat(preco4.value) || 0;
-    total4.textContent = (qtd * preco).toFixed(2); //tofixed() determina a qtd de casas decimais que desejo representar no numero
-    atualiizarvalorFinal();
-}
-
-function atualiizarvalorFinal(){
-    const valor1 = parseFloat(total1.textContent) ||0;
-    const valor2 = parseFloat(total2.textContent) ||0;
-    const valor3 = parseFloat(total3.textContent) ||0;
-    const valor4 = parseFloat(total4.textContent) ||0;
-    ValorFinal.textContent = (valor1+valor2+valor3+valor4). toFixed(2);
-}
-
-
-
-quantidade1.addEventListener('input', calcularTotal1);
-preco1.addEventListener('input', calcularTotal1);
-
-quantidade2.addEventListener('input', calcularTotal2);
-preco2.addEventListener('input', calcularTotal2);
-
-quantidade3.addEventListener('input', calcularTotal3);
-preco3.addEventListener('input', calcularTotal3);
-
-quantidade4.addEventListener('input', calcularTotal4);
-preco4.addEventListener('input', calcularTotal4);
